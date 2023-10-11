eafba2df1a85f6fdfe7d6caa42bd7edac5ee6eff
shubham <shubham71101@gmail.com>
2023-10-11 22:55:01 +0530

    angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home" , component:HomeComponent},
+  {path:"about" , component:AboutComponent},
+  {path:"blog" , component:BlogsComponent},
+  {path:"contact" , component:ContactUsComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
